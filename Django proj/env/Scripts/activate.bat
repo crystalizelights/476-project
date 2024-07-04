a2e4cd6741ffcd0bd5114ec61333844e98299b5c
mkamya20 <mkamya0@gmail.com>
2024-07-04 14:14:48 +0300

    Addition of item app
    Added item app to be able to view individual items of stores. I also changed the collour of footer and header

--- Django proj/env/Scripts/activate.bat
+++ Django proj/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\MOSES\Desktop\IDRC 2024\Django proj\env
+set VIRTUAL_ENV=C:\Users\MOSES\Desktop\Summer 2024\CS 476\476-project\Django proj\env
 
 if not defined PROMPT set PROMPT=$P$G
 
