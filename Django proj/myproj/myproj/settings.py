b4a889173c4630186cccbdb66483b9138eb7a8d4
David Elumeze <54193310+CjDave@users.noreply.github.com>
2024-07-16 01:58:30 -0600

    Setup Project For Web Hosting

--- Django proj/myproj/myproj/settings.py
+++ Django proj/myproj/myproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y^hk#u@_5+4@)yjwc*kzk!lcqxmgj7gi_f-_d_3fg-doczka4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "dce143.pythonanywhere.com"]
 
 LOGIN_REDIRECT_URL ='/'
 LOGOUT_REDIRECT_URL = '/'
